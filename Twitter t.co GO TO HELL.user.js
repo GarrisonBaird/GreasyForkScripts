24f9d9d40906c7700bea6a6660681dc70fb9cd3a
GarrisonBaird <garrisonbairdny7@outlook.com>
2017-03-28 14:28:30 +0000

    Create Twitter t.co GO TO HELL.user.js

---
+++ Twitter t.co GO TO HELL.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Twitter t.co GO TO HELL
+// @description Restore t.co links, support Twitter web/mobile/tweetdeck
+// @include     twitter.com
+// @match       *://*.twitter.com/*
+// @version     1.0
+// @grant       none
+// @namespace   https://greasyfork.org/users/113252-garrison-baird
+// ==/UserScript==
+
+
+function main () {
+	Array.prototype.slice.call(window.document.querySelectorAll('a[href*="t.co"]'), 0) .forEach(function (el) {
+		if (el.dataset && el.dataset.expandedUrl) { // Twitter web
+			el.href = el.dataset.expandedUrl;
+		}
+		if (el.dataset && el.dataset.fullUrl) { // TweetDeck
+			el.href = el.dataset.fullUrl;
+		}
+		if (el.children.length > 0 && el.children[0].tagName == "SPAN" && el.children[0].innerText.startsWith("(link: ")) {
+			// Fucking Twitter mobile (https://mobile.twitter.com/)
+			// el.children[0].innerText == "(link: https://www.google.com/) "
+			var href = el.children[0].innerText.trim();
+			href = href.substring("(link: ".length, href.length - ")".length);
+			el.href = href;
+		}
+	});
+};
+
+main();
+if (window.location.host == 'tweetdeck.twitter.com') { // TweetDeck won't trigger 'scroll' event
+  setInterval(main, 1000);
+} else {
+  window.addEventListener('scroll', main);
+}
